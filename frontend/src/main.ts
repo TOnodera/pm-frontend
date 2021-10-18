4c3a6be4bba06b73a7993c4d390731d8eb119552
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-10-18 13:26:52 +0000

    primevueの設定完了

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import PrimeVue from 'primevue/config';
+import 'primevue/resources/themes/saga-blue/theme.css'; //theme
+import 'primevue/resources/primevue.min.css'; //core css
+import 'primeicons/primeicons.css'; //icons
 
-createApp(App).use(router).mount('#app');
+createApp(App).use(router).use(PrimeVue).mount('#app');
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -2,17 +2,20 @@
     <div class="home">
         <img alt="Vue logo" src="../assets/logo.png" />
         <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+        <Button label="primevue" />
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Button from 'primevue/button';
 
 export default defineComponent({
     name: 'Home',
     components: {
         HelloWorld,
+        Button,
     },
 });
 </script>
